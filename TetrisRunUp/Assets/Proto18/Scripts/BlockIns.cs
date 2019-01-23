01073a0e2f88476ab4e0427208c259acf5c246ba
SayaMiyagi <c18018@std.it-college.ac.jp>
2019-01-23 13:31:19 +0900

    ブロックをランダムに生成するプログラム追加。

---
+++ TetrisRunUp/Assets/Proto18/Scripts/BlockIns.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockIns : MonoBehaviour {
+
+    public GameObject[] block;
+    public GameObject insPos;
+
+    int nowBlock;
+
+    int display;
+
+	void Start () {
+        nowBlock = Random.Range(0, block.Length);
+        InsBlock();
+        display = NextBlock();
+
+        Debug.Log(display + " : " + nowBlock);
+    }
+	
+	/*void Update () {
+        InsBlock();
+
+        Debug.Log(display + " : " + nowBlock);
+	}*/
+
+    int NextBlock()
+    {
+        int i = Random.Range(0, block.Length);
+
+        return i;
+    }
+
+    void InsBlock()
+    {
+        nowBlock = display;
+        Instantiate(block[nowBlock], insPos.transform.position, Quaternion.identity);
+
+        display = NextBlock();
+    }
+}
