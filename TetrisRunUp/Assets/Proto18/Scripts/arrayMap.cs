5eff749c6bdaebfc078532edfe8e80a44120cd02
SayaMiyagi <c18018@std.it-college.ac.jp>
2019-01-24 08:47:36 +0900

    解像度を1920*1200に変更

---
+++ TetrisRunUp/Assets/Proto18/Scripts/arrayMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrayMap : MonoBehaviour {
+
+    int[,] map = new int[20, 12];
+    
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
