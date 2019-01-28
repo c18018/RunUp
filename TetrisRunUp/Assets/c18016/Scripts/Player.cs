197047cf4cdb0ea5226a88c88e873c5a189ddfbe
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-01-28 17:06:41 +0900

    キャラクターの動き

---
+++ TetrisRunUp/Assets/c18016/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    //Player move speed
+    public float speed;
+
+    //Player Rigidbody
+    private Rigidbody rigid;
+
+    Vector3 playerPos;
+    public float forceSpeed;
+
+    //Player position
+    public float x;
+    public float y;
+    public float z;
+
+    public static bool isTouch;
+    public static bool isDestroy;
+
+    // Use this for initialization
+    void Start () {
+        rigid = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+        Debug.Log(isDestroy);
+    }
+
+    void PlayerMove()
+    {
+        //x = Input.GetAxis("Vertical");
+        //z = Input.GetAxis("Vertical");
+        z = 1;
+
+        playerPos = new Vector3(x, y, z) * speed;
+
+        transform.Translate(playerPos * Time.deltaTime);
+
+        if (isTouch == true)
+        {
+            rigid.AddForce(transform.up * forceSpeed);
+
+        }
+
+        if(isDestroy == true)
+        {
+            
+        }
+    }
+}
---
+++ TetrisRunUp/Assets/c18016/Scripts/PlayerCollider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            Player.isTouch = true;
+            //Debug.Log(Player.isTouch);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            if (Player.isDestroy == true)
+            {
+                Destroy(other.gameObject);
+                Player.isTouch = false;
+                Player.isDestroy = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player.isTouch = false;
+    }
+
+    //DB = DestroyButton
+    public void PointerUp()
+    {
+        Player.isDestroy = false;
+    } 
+
+    public void PointerDown()
+    {
+        Player.isDestroy = true;
+    }
+}
