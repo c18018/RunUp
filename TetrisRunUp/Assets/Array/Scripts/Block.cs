006a58d486718c2c0e358ecef2408b300640fc28
SayaMiyagi <c18018@std.it-college.ac.jp>
2019-02-05 10:17:27 +0900

    他の方法

--- TetrisRunUp/Assets/Array/Scripts/Block.cs
+++ TetrisRunUp/Assets/Array/Scripts/Block.cs
@@ -4,34 +4,21 @@ using UnityEngine;
 using System;
 
 public class Block : MonoBehaviour {
+    
 
-    /*bool next;
-    Rigidbody gravity;
-
-    private void Start()
-    {
-        gravity = GetComponent<Rigidbody>();
-    }
-
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "cube2")
         {
             gameObject.tag = "cube2";
-            next = true;
         }
-    }
-
-    public bool Next()
-    {
-        return next;
-    }
+    }*/
 
     private void OnCollisionExit(Collision collision)
     {
         if(collision.gameObject.tag == "cube2")
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
         }
-    }*/
+    }
 }
